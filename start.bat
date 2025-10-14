4a6fe9e64478d10fabf22a2867c66d53cb0cd974
EBox <EmptyBox36@users.noreply.github.com>
2025-10-15 02:36:17 +0700

    Edit start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,11 @@
 @echo off
+set /p choice="Run git pull before starting? (y/n): "
 
-git pull
+if /i "%choice%"=="y" (
+    git pull
+) else (
+    echo Skipping git pull...
+)
 
 python main.py
+pause
